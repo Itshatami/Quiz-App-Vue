2c16cb3eb0528bb26a15635b888859fec90878d8
Alireza Hatami <itshatami@gmail.com>
2023-11-20 09:11:18 +0330

    first initial

---
+++ quiz/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>hello</h1>
+</template>
+
+<script setup>
+
+</script>
+
+<style >
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+body {
+  background-color: #264653;
+  color: #fff;
+}
+</style>
